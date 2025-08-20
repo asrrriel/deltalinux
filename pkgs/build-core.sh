841a2ed6ee95450e02a5001c4943e83259093c58
lolguy91 <retek1544@gmail.com>
2025-08-20 18:53:13 +0200

    fixed the fix to the fakeroot issue

--- pkgs/build-core.sh
+++ pkgs/build-core.sh
@@ -6,7 +6,7 @@ mkdir -p build
 mkdir -p done
 
 for pkg in buildscripts/*; do
-        if [ "buildscripts/util-linux.sh" -eq $pkg ]; then
+        if [ "buildscripts/util-linux.sh" == $pkg ]; then
                 fakeroot sh $pkg
         else
                 sh $pkg
