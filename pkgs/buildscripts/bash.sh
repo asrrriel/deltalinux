ab6e27ccd221e90c918e5943f9c6dbae83d9f7f4
lolguy91 <retek1544@gmail.com>
2025-08-20 16:10:17 +0200

    silly symlinking mistake

--- pkgs/buildscripts/bash.sh
+++ pkgs/buildscripts/bash.sh
@@ -8,6 +8,8 @@ cd tmp/bash
 make -j$(nproc)
 make -j$(nproc) DESTDIR=$(realpath ../../build/bash) install
 
+ln -sf ./bash $(realpath ../../build/bash)/usr/bin/sh
+
 cd ../..
 
 tar -czf done/bash.tar.gz -C build/bash .
