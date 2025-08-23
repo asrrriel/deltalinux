04e0c6775d6ecdb0a53a013b96bbd45d1e5ca8fd
lolguy91 <retek1544@gmail.com>
2025-08-23 16:50:07 +0200

    custom logo

--- pkgs/buildscripts/fastfetch.sh
+++ pkgs/buildscripts/fastfetch.sh
@@ -2,7 +2,12 @@
 set -e
 
 mkdir -p build/fastfetch
+
+cp ../configs/logo.txt tmp/fastfetch/src/logo/ascii/deltalinux.txt
+
 cd tmp/fastfetch
+
+git apply ../../../patches/logo.patch
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr ..
